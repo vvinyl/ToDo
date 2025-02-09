f18c0c36e5e53050cc1ee1ecb73cb9782c412122
vvinyl <jakubplyta01@gmail.com>
2025-02-10 00:07:08 +0100

    sth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Orb from './ReactBits/Orb/Orb';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100%', height: '600px', position: 'relative' }} className="orb-container">
+    <Orb
+      hoverIntensity={0.5}
+      rotateOnHover={true}
+      hue={0}
+      forceHoverState={false}
+    />
+  </div>
     </div>
   );
 }
