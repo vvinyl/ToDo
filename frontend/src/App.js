3c0ad35b1e6223f3dd079e77d07978c44ff12f6e
vvinyl <jakubplyta01@gmail.com>
2025-02-11 21:44:49 +0100

    libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Orb from './ReactBits/Orb/Orb';
 
+
 function App() {
   return (
     <div className="App">
-    <div style={{ width: '100%', height: '600px', position: 'relative' }} className="orb-container">
-    <Orb
-      hoverIntensity={0.5}
-      rotateOnHover={true}
-      hue={0}
-      forceHoverState={false}
-    />
-  </div>
+    
     </div>
   );
 }
---
+++ frontend/src/pages/AppLoaded/AppLoaded.js
@@ -0,0 +1,6 @@
+
+export default AppLoaded = () => {
+    return(
+        abcd
+    )
+}
